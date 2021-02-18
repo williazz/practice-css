9902acb59591f669a53d323ff5abe32fd454f1e3
William Zhou <williamz.zhou1@gmail.com>
2021-02-17 20:55:46 -0800

    grid-auto-rows

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,22 +3,19 @@ import _ from 'lodash';
 import faker from 'faker';
 import React from 'react';
 
-const components = [
-  { type: 'header' },
-  { type: 'content' },
-  { type: 'sidebar', content: faker.lorem.paragraph(5) },
-  { type: 'footer' },
-];
+// const components = [
+//   { type: 'header' },
+//   { type: 'content' },
+//   { type: 'sidebar', content: faker.lorem.paragraph(5) },
+//   { type: 'footer' },
+// ];
 
 const Grid = () => {
   return (
     <div className="grid-component">
-      {components.map(({ type, content }, n) => (
-        <div
-          key={`${type}-${n}`}
-          className={[type, 'grid-component-item'].join(' ')}
-        >
-          {type}: {content}
+      {_.range(20).map((n) => (
+        <div className="grid-component-item" key={`grid-component-item-${n}`}>
+          {n}
         </div>
       ))}
     </div>
